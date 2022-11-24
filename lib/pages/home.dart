82869653e52fd51d4423c906dc32f9e0cd8460dd
Aastha Sinha <aasthasinha2010@gmail.com>
2022-11-25 01:30:54 +0530

    homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,29 +1,55 @@
 import 'package:aastha/widgets/card.dart';
+import 'package:aastha/widgets/navigator.dart';
 import 'package:aastha/widgets/tags.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    Text(
+      'Index 1: Saved',
+    ),
+    Text(
+      'Index 2: Explore',
+    ),
+    Text(
+      'Index 3: Forum',
+    ),
+    Text(
+      'Index 4: Profile',
+    ),
+  ];
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'YANA',
-          style: TextStyle(color: Colors.black, fontSize: 30),
+        title: Padding(
+          padding: const EdgeInsets.only(top:30),
+          child: Image.asset(
+            'assets/logo.png',
+            fit: BoxFit.cover,
+            height: 120,
+          ),
         ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: const Icon(Icons.menu, color: Colors.black),
         actions: const [
-          Icon(
-            Icons.notifications,
-            color: Colors.black,
+          Padding(
+            padding: EdgeInsets.only(right: 12.0),
+            child: Icon(
+              Icons.notifications,
+              color: Colors.black,
+            ),
           )
         ],
       ),
+      // body: Center(
+      //   child: _widgetOptions.elementAt(_selectedIndex),
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Center(
@@ -43,7 +69,7 @@ class HomePage extends StatelessWidget {
               const Align(
                 alignment: Alignment.topLeft,
                 child: Padding(
-                  padding: EdgeInsets.all(10),
+                  padding: EdgeInsets.only(top: 14, left: 14, right: 14),
                   child: Text(
                     'Top Picks For You',
                     style: TextStyle(fontSize: 25),
@@ -66,6 +92,7 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: const CustomNav(),
     );
   }
 }
---
+++ lib/widgets/navigator.dart
@@ -0,0 +1,67 @@
+import 'package:aastha/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class CustomNav extends StatefulWidget {
+  const CustomNav({super.key});
+
+  @override
+  State<CustomNav> createState() => _CustomNavState();
+}
+
+class _CustomNavState extends State<CustomNav> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    Text(
+      'Index 1: Saved',
+    ),
+    Text(
+      'Index 2: Explore',
+    ),
+    Text(
+      'Index 3: Forum',
+    ),
+    Text(
+      'Index 4: Profile',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Saved',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Explore',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_outlined),
+          label: 'Forum',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outlined),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      unselectedIconTheme: const IconThemeData(color: Colors.black),
+      unselectedLabelStyle: const TextStyle(color: Colors.black),
+      selectedItemColor: const Color(0xFF98DAD9),
+      onTap: _onItemTapped,
+    );
+  }
+}
